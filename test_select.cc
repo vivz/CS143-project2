acd487485ee489287e9d7fcea15ae9ab3a7fef06
vivz <vivianzhang61@gmail.com>
2016-11-27 13:48:06 -0800

    what's wrong with my test.sh

--- test_select.cc
+++ test_select.cc
@@ -17,15 +17,21 @@ int main(){
 	
 	SelCond a;
 	a.attr = 1;
-	a.comp = SelCond::LT;
-	string a_value = "2500";
+	a.comp = SelCond::GT;
+	string a_value = "100";
 	a.value = (char*)a_value.c_str();
+
+	SelCond b;
+	b.attr = 1;
+	b.comp = SelCond::LT;
+	string b_value = "500";
+	b.value = (char*)b_value.c_str();
 	
 	std::vector<SelCond> cond;
 	
-	//cond.push_back(a);
-	
+	cond.push_back(a);
+	cond.push_back(b);
 
-	SqlEngine::select(4, "small", cond);
+	SqlEngine::select(3, "small", cond);
 	return 0;
 }
